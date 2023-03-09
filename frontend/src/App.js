766b311e6b4da42d1dde0642c2d3e4b4908db384
trupti0406 <truptivijayyadav27@gmail.com>
2023-03-09 19:39:31 +0530

    initialising repo

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">E-Store</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
