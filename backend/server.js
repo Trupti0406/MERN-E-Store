7cd92e02795fba78a15be9ba1395cea6b082acbe
trupti0406 <truptivijayyadav27@gmail.com>
2023-03-13 02:21:57 +0530

    created mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const port = process.env.PORT || 5000;
+
+// To fectch variables from .env file
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 app.get("/api/products", (req, res) => {
