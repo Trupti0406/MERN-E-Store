addaa5671ae33eccbb6dc3bf1ed298687d18b382
trupti0406 <truptivijayyadav27@gmail.com>
2023-04-25 23:48:12 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -124,7 +124,7 @@ function App() {
                       </ul>
                     </div>
                   ) : (
-                    <Link className="btn nav-link" to="/signin">
+                    <Link className="btn nav-link btn-warning" to="/signin">
                       Sign In
                     </Link>
                   )}
