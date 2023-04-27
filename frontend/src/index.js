8fa102e341176efb6c6c7b1a010affc9a3494f62
trupti0406 <truptivijayyadav27@gmail.com>
2023-04-28 01:10:03 +0530

    Both backend and frontend completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import {PayPalScriptProvider} from '@paypal/react-paypal-js'
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { StoreProvider } from "./Store";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools(); //disabeling devtools at the production stage
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,8 +21,3 @@ root.render(
     </StoreProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
